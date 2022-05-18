4077dedda245a70ad44ffe0654a5fb7bf2c47976
Maniacjoy <52311737+maniacjoy@users.noreply.github.com>
2022-05-18 15:05:32 -0700

    added: mutliple routers and scroll to top

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App'
-import './index.css'
+import ScrollToTop from './components/common/ScrollToTop';
+import About from './components/pages/about/About';
+import AppDesign from './components/pages/app-design/AppDesign';
+import Contact from './components/pages/contact/Contact';
+import GraphicDesign from './components/pages/graphic-design/GraphicDesign';
+import Location from './components/pages/location/Location';
+import WebDesign from './components/pages/web-design/WebDesign';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(
+  <BrowserRouter>
+    <ScrollToTop />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/location" element={<Location />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/web-design" element={<WebDesign/>} />
+      <Route path="/app-design" element={<AppDesign />} />
+      <Route path="/graphic-design" element={<GraphicDesign />} />
+    </Routes>
+  </BrowserRouter>
 )
